85f16af25b512c55169dec8033b8d9821dfbdd47
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2022-05-25 18:00:08 +0700

    Membuat Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './css/App.css';
+import { Modal } from 'react-bootstrap';
 
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="App">
+    <div className="container-fluid">
       <div>
         <div>
-
           <h1 className="ml12">7 Days Student</h1>
         </div>
 
@@ -15,7 +21,28 @@ function App() {
             <button className="button-30 btn btn-primary center-block">Start</button>
           </div>
           <div id='center'>
-            <button className="button-30">About us</button>
+            <button className="button-30" onClick={handleShow} >About us</button>
+            <Modal
+              show={show}
+              onHide={handleClose}
+              backdrop="static"
+              keyboard={false}
+              id="modal"
+            >
+              <Modal.Header closeButton id="modalHeader">
+                <Modal.Title>LOGO KITA</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <p className='container-fluid'>Kelompok 3</p>
+                <p className='mx-5'>- Christian Ivan Wibowo (00000058450)</p>
+                <p className='mx-5'>- Stanislaus Vieri Halim (00000057445)</p>
+                <p className='mx-5'>- James Bernard William (00000059431)</p>
+                <p className='mx-5'>- Dhena Purnama (00000058902)</p>
+              </Modal.Body>
+              <Modal.Footer>
+                <p>version 16.15.0 LTS</p>
+              </Modal.Footer>
+            </Modal>
           </div>
           <div id='center'>
             <button className="button-30">Exit</button>
@@ -28,10 +55,8 @@ function App() {
 
 
 
-
-
-
       </div>
+
     </div>
   );
 }
