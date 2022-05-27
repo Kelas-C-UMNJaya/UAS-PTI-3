13a8bbeaec400012496d6f5432e39228a7074f60
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2022-05-27 10:58:46 +0700

    Buat Page Lulus

---
+++ src/Lulus.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./css/Lulus.css"
+
+function Lulus() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1 className="ml12">7 Days Student</h1>
+                </div>
+                <div>
+                    <h1 className="ml6">Selamat Anda Telah Lulus</h1>
+                </div>
+                <div id='center'>
+                    <button className="button-30 ">Home</button>
+                    <button className="button-30 ">Exit</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Lulus;
