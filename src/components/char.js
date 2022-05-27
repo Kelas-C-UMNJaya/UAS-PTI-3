56a4e3393c6422f37858aaa35181d07562ddb580
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2022-05-27 21:58:57 +0700

    Buat Carousel dan Karakter

---
+++ src/components/char.js
@@ -0,0 +1,11 @@
+const char = [
+    {
+        id: 1,
+        imgURL: "https://cdn.discordapp.com/attachments/979742486807322705/979746440052105236/unknown.png"
+    },
+    {
+        id: 2,
+        imgURL: "https://cdn.discordapp.com/attachments/979742486807322705/979748604761735168/unknown.png"
+    },
+];
+export default char;
--- src/menuChar.jsx
+++ src/menuChar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import "./css/menuChar.css";
 import Form from 'react-bootstrap/Form';
-import "./css/menuChar.css"
+import Carousel from 'react-bootstrap/Carousel';
+
 
 function menuChar() {
     return (
@@ -10,9 +12,51 @@ function menuChar() {
             </div>
             <hr />
             <div>
-                {/* TODO:  Carousel Karakter*/}
+                {/* TODO: Kurang Gambar */}
+
+                <Carousel fade id='char'>
+                    <Carousel.Item>
+                        <img
+
+                            className="d-block w-100"
+                            src="https://cdn.discordapp.com/attachments/979742486807322705/979746440052105236/unknown.png"
+                            alt="First slide"
+                        />
+                        {/* <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption> */}
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+
+                            className="d-block w-100"
+                            src="https://cdn.discordapp.com/attachments/979742486807322705/979748604761735168/unknown.png"
+                            alt="Second slide"
+                        />
+
+                        {/* <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption> */}
+                    </Carousel.Item>
+                    {/* <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=Third slide&bg=20232a"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item> */}
+                </Carousel>
+
+
             </div>
-            <div>
+            <div className='mt-4'>
                 <input type="text" placeholder='Enter Your Name Here' />
             </div>
             <div className='mt-3'>
